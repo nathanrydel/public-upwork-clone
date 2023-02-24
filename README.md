6ba968f9a9a4e9920129b9bfdd2923578503bc5c
Nathan Rydel <nathanrydel@gmail.com>
2023-02-24 11:34:35 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# public-upwork-clone
+An attempt to re-create the upwork site in React to act as a living resume
---
+++ src/jobs.js
@@ -0,0 +1,82 @@
+const JOBS = [
+  {
+    id: 1,
+    title: 'Technical Support Engineer',
+    location: 'Phoenix, AZ (Remote)',
+    remote: true,
+    paymentVerified: true,
+    company: 'Mend',
+    postedTime: 'Sep 2022 - Jan 2023',
+    description: `●	Tracked, documented, & reproduced client issues in production environments (including Windows, macOS, and Linux) via diagnostic log analysis or screenshare sessions to provide solutions or devise workarounds.
+    ●	Provided Technical Support for Software Composition Analysis (SCA) scanning and Static Application Security Testing (SAST) applications and their external integrations with leading SDLC technologies like GitHub, Docker, Azure DevOps, Jira, Jenkins, Bitbucket, GitLab, and many others.
+    ●	Collaborated cross-functionally with R&D and Product Management via Jira and Salesforce to address customer inquiries and facilitate successful resolution within customer contracted SLA.
+    ●	Exceeded Q4 2022 Personal Development and KPI goals by demonstrating my proficiency with the product’s capabilities and ability to answer technical product questions.`,
+    category: 'Most Recent Position'
+  },
+  {
+    id: 2,
+    title: 'Developer Operations Platform Senior Associate',
+    location: 'Austin, TX (Remote)',
+    remote: true,
+    paymentVerified: true,
+    company: 'Accenture',
+    postedTime: 'Jan 2022 - Jun 2022',
+    description: `● Promoted to SME/Tier 2 Role
+    ●	Supported multiple teams across workflows as a Subject Matter Expert and an escalation point for edge cases.
+    ●	Led one-on-one coaching sessions with associates to identify areas of improvement.
+    ●	Mentored fellow team members & track progress to help them achieve 95% Accuracy and 90% Consistency performance while increasing team efficiency.`,
+    category: 'DevOps Support'
+  },
+  {
+    id: 3,
+    title: 'Developer Operations Platform Associate',
+    location: 'Austin, TX (Remote)',
+    remote: true,
+    paymentVerified: true,
+    company: 'Accenture',
+    postedTime: 'Aug 2022 - Dec 2021',
+    description: `●	Exceeded 95%+ Accuracy and 90%+ Consistency metrics in 4 of 5 quarterly review periods.
+    ●	Performed app analysis and review in multiple workflows for the Developer Operations Policy team.
+    ●	Completed 3rd party app integration and compliance review within client case review system.`,
+    category: 'DevOps Support'
+  },
+  {
+    id: 4,
+    title: 'AppleCare Senior Technical Advisor',
+    location: 'Austin, TX',
+    remote: true,
+    paymentVerified: true,
+    company: 'Apple',
+    postedTime: 'June 2019 - July 2020',
+    description: `●	Acted as an escalation point for internal Advisors for both Advanced Technical Troubleshooting and Customer Satisfaction reasons.
+    ●	Delivered 98% Customer Satisfaction as a personal point of contact for Customers when multiple interactions are required for issue investigation and resolution.
+    ●	Collaborated with Engineering and non-technical teams as the internal Case Owner for any issue that required cross-functional team handling.`,
+    category: 'Technical Support'
+  },
+  {
+    id: 5,
+    title: 'AppleCare Mac Plus Advisor',
+    location: 'Austin, TX',
+    remote: true,
+    paymentVerified: true,
+    company: 'Apple',
+    postedTime: 'July 2018 - June 2019',
+    description: `●	Attained 96% Customer Satisfaction over 9 of 10 review periods with 2 of those being at 100% satisfaction
+    ●	Achieved Goal of 90% First Contact Issue Resolution during Customer interactions over 8 of 10 review periods while role average failed to meet the Goal 10 times during the same review period.`,
+    category: 'Technical Support'
+  },
+  {
+    id: 6,
+    title: 'AppleCare Technical Advisor',
+    location: 'Austin, TX',
+    remote: true,
+    paymentVerified: true,
+    company: 'Apple',
+    postedTime: 'Jan 2018 - July 2018',
+    description: `●	Applied the PAIR (Probe, Analyze, Isolate and Resolve) troubleshooting method to a variety of complex technical issues in the iOS environment, while tailoring the experience to each customer
+    ●	“Exceeded” Key Performance Indicator such as Average Handle Time and First Contact Issue Resolution metrics consistently while providing a high level of customer service reflected in Very Satisfied survey responses`,
+    category: 'Customer Support'
+  }
+];
+
+export default JOBS;
